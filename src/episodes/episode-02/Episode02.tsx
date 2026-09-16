78eda19c44df06cfef1cdd1712ceabb4bd9cf5b6
潘家杰 <panjiajie@chexiao.co>
2026-09-16 11:51:56 +0800

    第二期的封面

--- src/episodes/episode-02/Episode02.tsx
+++ src/episodes/episode-02/Episode02.tsx
@@ -1,8 +1,16 @@
-import { AbsoluteFill, Audio, staticFile } from "remotion";
+import { AbsoluteFill, Audio, Sequence, staticFile } from "remotion";
+import { ProgressBar, SeriesIntro, SERIES_INTRO_DURATION } from "../../components";
 
 /** 第 2 期《你的孩子的“熊”来自哪里》时间线入口。字幕由 TXT 文稿在后期手动加入。 */
 export const Episode02 = () => (
   <AbsoluteFill>
+    <Sequence durationInFrames={SERIES_INTRO_DURATION}>
+      <SeriesIntro
+        emphasisLines={[1]}
+        title={["你的孩子的", "“熊”", "来自哪里"]}
+      />
+    </Sequence>
     <Audio src={staticFile("episodes/episode-02/2.m4a")} />
+    <ProgressBar />
   </AbsoluteFill>
 );
